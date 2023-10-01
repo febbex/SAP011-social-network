9421946c131c623caef2a3568691cbecd0986a1c
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-10-01 14:42:14 -0300

    adicionado post na pag feed + ajuste css

--- src/lib/feed/feed.js
+++ src/lib/feed/feed.js
@@ -1,8 +1,8 @@
 import { sair } from "../../services/firebaseLogin.js";
 
 export default () => {
-    const container = document.createElement('div');
-    const template = `
+  const container = document.createElement('div');
+  const template = `
     <section id="feed">
     <header>
     <a class=userHeader>
@@ -13,24 +13,107 @@ export default () => {
     </header>
     
     
-    <section id="container">
-    <section id="input-container">
-    <input type="text" name="post" placeholder="Como foi sua viagem?" id="postPlace"/>
-    <p><input id="publicar" type="submit" value="Publicar"></p>
-    </section>
-    </section>
-    </section>
+    <main class="main">
+    <div class="newPost">
+    <div class="infoUser">
+    <div class="imgUser"></div>
+    <strong>Thamires Febbo</strong>
+    </div>
 
-      
+    <form action="" class="formPost">
+    <textarea name="textarea" placeholder="Como foi sua viagem?"></textarea>
+
+    <div class="iconsAndBtn">
+  
+
+    <button id="publicar" type="submit" class="btnSubmitForm">Publicar</button>
+
+    </div> 
+    </form>
+    </div>
+    <ul class="posts">
+    <li class="post">
+        <div class="infoUserPost">
+            <div class="imgUserPost"></div>
+                    <strong class="name">
+                        Thamires Febbo
+                    </strong>
+                </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            Quis ipsum suspendisse ultrices gravida.
+            Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+
+        <div class="actionBtnPost">
+                <button class="btnPost">
+                    <img src="./assets/icon_curtida2.svg" alt="curtir">
+                </button>
+                <button class="btnPost">
+                    <img src="./assets/icon_edt2.svg" alt="editar">
+                </button>
+            </div>
+    </li>
+    <li class="post">
+           <div class="infoUserPost">
+            <div class="imgUserPost"></div>
+                    <strong class="name">
+                        Thamires Febbo
+                    </strong>
+                </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            Quis ipsum suspendisse ultrices gravida.
+            Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+
+        <div class="actionBtnPost">
+                <button class="btnPost">
+                    <img src="./assets/icon_curtida2.svg" alt="curtir">
+                </button>
+                <button class="btnPost">
+                    <img src="./assets/icon_edt2.svg" alt="editar">
+                </button>
+            </div>
+    </li>
+
+    <li class="post">
+        <div class="infoUserPost">
+            <div class="imgUserPost"></div>
+            <strong class="name">
+            Thamires Febbo
+        </strong>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            Quis ipsum suspendisse ultrices gravida.
+            Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+
+        <div class="actionBtnPost">
+                <button class="btnPost">
+                    <img src="./assets/icon_curtida2.svg" alt="curtir">
+                </button>
+                <button class="btnPost">
+                    <img src="./assets/icon_edt2.svg" alt="editar">
+                </button>
+            </div>
+    </li>
+</ul>
+
+    </main>
+
+  
 
     </form>
-    </section>`;  
+    </section>`;
 
 
-  container.innerHTML = template; 
+  container.innerHTML = template;
 
   const btnSair = container.querySelector("#btnSair");
   btnSair.addEventListener("click", sair);//chama a função sair, ao clicar no btn sair
 
   return container;
-  }
+}
+
+
