6e05bc384ceec9ae1a8813e176876c25145c74d2
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-10-26 15:12:49 -0300

    voltando 3commits

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKrkRCYDujEg31zlOisPxj4Z5xBqpLiLM",
-  authDomain: "mytrip-ba618.firebaseapp.com",
-  projectId: "mytrip-ba618",
-  storageBucket: "mytrip-ba618.appspot.com",
-  messagingSenderId: "349173921736",
-  appId: "1:349173921736:web:84b64ab17340c23c5db2fd"
+  apiKey: 'AIzaSyCKrkRCYDujEg31zlOisPxj4Z5xBqpLiLM',
+  authDomain: 'mytrip-ba618.firebaseapp.com',
+  projectId: 'mytrip-ba618',
+  storageBucket: 'mytrip-ba618.appspot.com',
+  messagingSenderId: '349173921736',
+  appId: '1:349173921736:web:84b64ab17340c23c5db2fd',
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
